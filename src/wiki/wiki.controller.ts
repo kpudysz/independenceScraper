bb65e908b5e4cb07303337f5211b99d051532ba6
kpudysz <krzysztofpudysz97@gmail.com>
2025-05-14 23:41:34 +0200

    feat: removed throttler

--- src/wiki/wiki.controller.ts
+++ src/wiki/wiki.controller.ts
@@ -1,14 +1,11 @@
 import { Controller, Post, Body, Res } from '@nestjs/common'
 import { MailerService } from '@nestjs-modules/mailer'
-import { Response } from 'express'
-import { Throttle } from '@nestjs/throttler'
 
 @Controller('wiki')
 export class WikiController {
   constructor(private readonly mailerService: MailerService) {}
 
   @Post('send-suggestion')
-  @Throttle({ default: { limit: 5, ttl: 3600 } })
   async sendSuggestion(
     @Body('content') content: string,
     @Body('source') source: string,
@@ -21,9 +18,9 @@ export class WikiController {
         subject: `New Wiki Suggestion for ${source} by ${author}`,
         text: `Content: ${content}`
       })
-      return res.status(200).send({ status: 'ok' })
+      return res.status
     } catch (error) {
-      return res.status(500).send({ status: 'error', message: error as string })
+      return res.status
     }
   }
 }
